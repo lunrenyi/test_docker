1c7a9be0582e1108b80a4bcab1a1e33eeeb9d4a8
lunrenyi <87307989+lunrenyi@users.noreply.github.com>
2023-03-29 22:05:48 +0800

    Update test_bash.sh

--- test_bash.sh
+++ test_bash.sh
@@ -2,13 +2,9 @@
 # 定义镜像名称
 IMAGE_NAME="hello_world"
 # 定义 Dockerfile 路径
-DOCKERFILE="./dockerfile"
+DOCKERFILE="."
 # 构建镜像
 echo "Building image..."
 docker image build -t "$IMAGE_NAME" "$DOCKERFILE"
-
-# 运行容器
-echo "Starting container..."
-docker run -p 5000:5000 --name=hello "$IMAGE_NAME"
-
+# 完成
 echo "Done."
